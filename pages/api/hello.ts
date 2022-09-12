3f43041a06257e9f2b30dc4306d71ef3309bbf76
Remus <remuskwan23@gmail.com>
2022-09-12 21:53:46 +0800

    fix: revert change

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('hello')
   res.status(200).json({ name: 'John Doe' })
 }
