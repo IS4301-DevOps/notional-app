a06546769784359069403bd0f0daf30fea5c1f53
Remus <remuskwan23@gmail.com>
2022-10-11 16:46:59 +0800

    style: updated styling of badge color and text

--- components/dashboard/BreakdownListItem.tsx
+++ components/dashboard/BreakdownListItem.tsx
@@ -1,5 +1,7 @@
 import { BreakdownByUser } from '../../interfaces';
 import _ from 'lodash';
+import classNames from '../../utils/classNames';
+import { CatColorsTailwind, CategoryLabel } from '../../interfaces/enums';
 
 type Props = {
   item: BreakdownByUser;
@@ -9,8 +11,14 @@ type Props = {
 const BreakdownListItem = ({ item, value }: Props) => {
   return (
     <li className='flex py-4'>
-      <span className='inline-flex items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-800'>
-        {item.category}
+      <span
+        className={classNames(
+          'inline-flex items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-800',
+          `text-${CatColorsTailwind[item.category]}-800`,
+          `bg-${CatColorsTailwind[item.category]}-100`,
+        )}
+      >
+        {CategoryLabel[item.category]}
       </span>
       <div className='flex justify-between w-full ml-3'>
         <p className='text-sm font-medium text-gray-900'>{_.startCase(_.toLower(item.category))}</p>
--- interfaces/enums.ts
+++ interfaces/enums.ts
@@ -5,3 +5,19 @@ export enum CategoryColors {
   BILL = 'grape',
   OTHERS = 'gray',
 }
+//TODO: extend tailwind color palette in tailwind.config.js
+export enum CatColorsTailwind {
+  FOOD = 'red',
+  GROCERIES = 'cyan',
+  TRANSPORT = 'orange',
+  BILL = 'purple',
+  OTHERS = 'gray',
+}
+
+export enum CategoryLabel {
+  FOOD = 'FOOD',
+  GROCERIES = 'GROC',
+  TRANSPORT = 'TRAN',
+  BILL = 'BILL',
+  OTHERS = 'OTHR',
+}
