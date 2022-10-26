152f7c87ad7f65dc0a04d4b45e8877af5fd251fc
keithcharlesc <keithcharleschan@gmail.com>
2022-10-27 02:21:48 +0800

    Fixed positioning issue of button

--- components/landing/ESGQuiz.tsx
+++ components/landing/ESGQuiz.tsx
@@ -6,7 +6,7 @@ export default function ESGQuiz() {
     <Link href='/esg-quiz'>
       <button
         type='button'
-        className='inline-flex items-center rounded-md border border-green-600 bg-white px-6 py-3 
+        className='inline-flex w-full place-content-center items-center rounded-md border border-green-600 bg-white px-6 py-3 
         text-base font-medium text-green-600 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2'
       >
         Take an ESG Quiz to test your knowledge!
